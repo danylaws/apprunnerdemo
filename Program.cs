63d80d3368b2397df94d18a8cc85a1492983847e
Teyi Daniel Lawson <danylaws@gmail.com>
2023-03-31 20:47:32 +0200

    edit message

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, world!");
+app.MapGet("/", () => "Hello, from AWS Summit Paris 2023!");
 
 app.Run();
